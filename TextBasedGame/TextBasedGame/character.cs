f6716df34cc9cb1c6986935b467e80f7328d9a32
Sentrymon <sennahoj04@gmail.com>
2020-09-07 12:33:42 +0200

    Added character

---
+++ TextBasedGame/TextBasedGame/character.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextBasedGame
+{
+    class character
+    {
+        private int
+            health = 20,
+            stamina = 20,
+            toughness = 0,
+            damageMax = 4,
+            damageMin = 2;
+
+    }
+}
--- TextBasedGame/TextBasedGame/main.cs
+++ TextBasedGame/TextBasedGame/main.cs
@@ -6,7 +6,10 @@ namespace TextBasedGame
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            for (; ; )
+            {
+
+            }
         }
     }
 }
